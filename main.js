de09484e23027fb98440550b11760e825eb52afa
rolopez23 <rob.lopez@gmail.com>
2020-04-25 09:40:23 -0700

    updates

--- main.js
+++ main.js
@@ -3,97 +3,10 @@ const waitOneSecond = require('./waitOneSecond');
 const writeToFile = require('./writeToFile');
 
 
-// getNumLessThanTen((error, success) => {
-//   if (error) {
-//     console.log('that shouldnot be right');
-//   } else {
-//     console.log(success);
-//   }
-// });
 
-// getNumLessThanTen((error, success) => {
-//   if (error) {
-//     console.log('that shouldnot be right');
-//   } else {
-//     waitOneSecond((error, success) => {
-//       if (error) {
-//         console.log('oh no');
-//       } else {
-//         console.log(success);
-//       }
-//     });
-//   }
-// });
-
-// waitOneSecond((error, success)=>{
-//   if (error) {
-//     console.log('what went wrong? 1')
-//   } else {   
-//     console.log(success)
-//     waitOneSecond((error, success)=> {
-//       if (error) {
-//         console.log('what went wrong? 2')
-//       } else {
-//         console.log(success);
-//         waitOneSecond((error, success) => {
-//           if (error) {
-//             console.log('uh oh you are on the inside');
-//           } else {
-//             console.log(success);
-//             getNumLessThanTen((error, success) => {
-//               if (error) {
-//                 console.log('something is going on');
-//               } else {
-//                 console.log('here is the random number ', success);
-//                 writeToFile(success, (suc) => {
-//                   if (suc) {
-//                     console.log(suc);
-//                   }
-//                 })
-//               }
-//             })
-//           }
-//         })
-//     }
-//     })
-//   }
-// });
 
 waitOneSecond((error, success) => {
-  if (error) {
-    console.log('ruh-roh');
-  } else {
-    console.log(success);
-    waitOneSecond((error, success) => {
-      if (error) {
-        console.log('uh-oh');
-      } else {
-        console.log(success);
-        waitOneSecond((error, success) => {
-          if (error) {
-            console.log(error)
-          } else {
-            console.log(success);
-            console.log('getting number now')
-            getNumLessThanTen((error, success) => {
-              if (error) {
-                console.log(error);
-              } else {
-                console.log('number received! it is ' + success);
-                writeToFile(success, (suc, err) => {
-                  if (err) {
-                    console.log('so close!');
-                  } else {
-                    console.log(suc);
-                  }
-                })
-              }
-            })
-          }
-        })
-      }
-    })
-  }
+ 
 })
 
 /*
